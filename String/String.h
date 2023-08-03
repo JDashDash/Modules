3d6e128e5d6561195cb221d48f5df776049b5c76
Program <67320703+Program132@users.noreply.github.com>
2023-08-03 12:26:32 +0200

    Update String.h

--- String/String.h
+++ String/String.h
@@ -9,4 +9,8 @@ namespace JDD::Modules::String {
     char getCharFromIndex(const std::string& str, int index);
     bool equals(const std::string& str1, const std::string& str2);
     bool equalsIgnoreCase(const std::string& str1, const std::string& str2);
+    bool isEmpty(const std::string& str1);
+    std::string toLowerCase(const std::string& str1);
+    std::string toUpperCase(const std::string& str1);
+    std::string replace(const std::string& str1, char x, char y);
 }
