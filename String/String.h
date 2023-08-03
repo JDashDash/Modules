2ba6facca6aa4597a0624f82bb508ba2e7474a30
Program <67320703+Program132@users.noreply.github.com>
2023-08-03 12:01:44 +0200

    Update String.h

--- String/String.h
+++ String/String.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <string>
+#include <algorithm>
 
 namespace JDD::Modules::String {
     std::string concat(const std::string& str1, const std::string& str2);
     int getIndexFromChar(const std::string& str, char x);
     char getCharFromIndex(const std::string& str, int index);
+    bool equals(const std::string& str1, const std::string& str2);
+    bool equalsIgnoreCase(const std::string& str1, const std::string& str2);
 }
